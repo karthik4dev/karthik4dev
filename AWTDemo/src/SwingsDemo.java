9c3cb3e9802db5a750a64f7f0adabf1e393bb252
Karthik4dev <karthikpnrao.97@gmail.com>
2022-02-17 23:04:39 +0530

    saved file

--- AWTDemo/src/SwingsDemo.java
+++ AWTDemo/src/SwingsDemo.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.*;
 
 import javax.swing.*;
@@ -10,12 +12,14 @@ public class SwingsDemo extends JFrame implements ActionListener {
 	int count =0 ;
 	JTextField jt = new JTextField("Count is ",10);
 	
+	
 	public SwingsDemo() {
 	b = new JButton("count");
 	l=new JLabel("Count is "+count);
+	
 	jt.setEditable(false);
 	b.addActionListener(this);
-	b.setDefaultCapable(true);
+	b.setDefaultCapable(true);b.setBackground(new Color(3,22,11));
 	setLayout(new FlowLayout());
 	
 	add(b);
